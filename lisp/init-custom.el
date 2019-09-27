11397a0107cc7db29443b39d1d305e6f7b15d6ed
shohi <oshohi@gmail.com>
2019-09-27 17:46:21 +0800

    add functions for kill buffers

--- lisp/init-custom.el
+++ lisp/init-custom.el
@@ -6,3 +6,17 @@
   "Kills the current buffer."
   (interactive)
   (kill-buffer (current-buffer)))
+
+;; kill all buffers
+;; https://stackoverflow.com/questions/3417438/close-all-buffers-besides-the-current-one-in-emacs
+(defun sk/kill-all-buffers ()
+  "Kill all buffers"
+  (interactive)
+  (mapc 'kill-buffer (buffer-list)))
+
+;; kill all-other buffers
+;; https://www.emacswiki.org/emacs/KillingBuffers
+(defun sk/kill-other-buffers ()
+  "Kill all other buffers."
+  (interactive)
+  (mapc 'kill-buffer (delq (current-buffer) (buffer-list))))
