17cbc3f6da729b1c49b474af1dc58ee27f22547d
shohi <oshohi@gmail.com>
2019-11-02 07:58:38 +0800

    update init-nim.el

--- lisp/init-nim.el
+++ lisp/init-nim.el
@@ -1,26 +1,27 @@
 ;; -*- coding: utf-8; lexical-binding: t; -*-
 ;; nim-lang setup, refer https://lupan.pl/dotemacs/
 
-(use-package lsp-mode
-  :ensure t
-  :commands lsp
-  :config
-  ;; Register `nimlsp` from https://github.com/PMunch/nimlsp
-  (add-to-list 'lsp-language-id-configuration '(nim-mode . "nim"))
-  (lsp-register-client
-   (make-lsp-client :new-connection (lsp-stdio-connection '("nimlsp"))
-                    :major-modes '(nim-mode)
-                    :server-id 'nim-ls)))
+;; (use-package lsp-mode
+;;   :ensure t
+;;   :commands lsp
+;;   :config
+;;   ;; Register `nimlsp` from https://github.com/PMunch/nimlsp
+;;   (add-to-list 'lsp-language-id-configuration '(nim-mode . "nim"))
+;;   (lsp-register-client
+;;    (make-lsp-client :new-connection (lsp-stdio-connection '("nimlsp"))
+;;                     :major-modes '(nim-mode)
+;;                     :server-id 'nim-ls)))
 
-(use-package highlight-indent-guides
-  :ensure t
-  :init
-  (setq highlight-indent-guides-method 'character))
+;; (use-package highlight-indent-guides
+;;   :ensure t
+;;   :init
+;;   (setq highlight-indent-guides-method 'character))
 
 (use-package nim-mode
   :ensure t
   :init
-  (add-hook 'nim-mode-hook 'highlight-indent-guides-mode))
+  ;; (add-hook 'nim-mode-hook 'highlight-indent-guides-mode)
+  )
 
 (provide 'init-nim)
 ;;; init-nim.el ends here.
