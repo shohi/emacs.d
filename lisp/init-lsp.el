cd5d4a289662453433581e1b5d17978e2f0f8ecd
shohi <oshohi@gmail.com>
2020-03-27 22:28:20 +0800

    update golang setup

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -3,13 +3,39 @@
 
 (use-package lsp-mode
   :ensure t
-  :commands lsp)
+  ;; uncomment to enable gopls http debug server
+  ;; :custom (lsp-gopls-server-args '("-debug" "127.0.0.1:0"))
+  :commands (lsp lsp-defered)
+  :config (progn
+	    ;; use flycheck, not flymake
+            (setq lsp-prefer-flymake nil)))
 
-(use-package company-lsp
-  :ensure t)
+;; optional - provides fancy overlay information
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode
+  :config (progn
+            ;; disable inline documentation
+            (setq lsp-ui-sideline-enable nil)
+            ;; disable showing docs on hover at the top of the window
+            (setq lsp-ui-doc-enable nil)))
 
 (use-package company
-  :ensure t)
+  :ensure t
+  :config (progn
+            ;; don't add any dely before trying to complete thing being typed
+            ;; the call/response to gopls is asynchronous so this should have little
+            ;; to no affect on edit latency
+            (setq company-idle-delay 0)
+            ;; start completing after a single character instead of 3
+            (setq company-minimum-prefix-length 1)
+            ;; align fields in completions
+            (setq company-tooltip-align-annotations t)
+            ))
+
+(use-package company-lsp
+  :ensure t
+  :commands company-lsp)
 
 (provide 'init-lsp)
 ;;; init-lsp.el ends here.
