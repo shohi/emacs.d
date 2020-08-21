481dc5d38bdf63b57c10b3bf07713913f462f4d6
shohi <oshohi@gmail.com>
2020-08-21 21:38:17 +0800

    update

--- lisp/init-custom.el
+++ lisp/init-custom.el
@@ -88,9 +88,9 @@
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
 
-(global-set-key "\C-w" 'backward-kill-word)
-(global-set-key "\C-x\C-k" 'kill-region)
-(global-set-key "\C-c\C-k" 'kill-region)
+(global-set-key "\C-c\C-d" 'backward-kill-word)
+;; (global-set-key "\C-x\C-k" 'kill-region)
+;; (global-set-key "\C-c\C-k" 'kill-region)
 
 (defalias 'qrr 'query-replace-regexp)
 
